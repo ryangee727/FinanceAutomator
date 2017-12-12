4f200655a0f160c229b8aef425425b85b670d515
ryan gee <geebaybee727@yahoo.com>
2017-12-11 17:38:43 -0800

    Finished Importer into AR Expenses. Added ArgParser for Steps

--- script/run.py
+++ script/run.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 import os, sys
 import pandas as pd
+import argparse
+
 # Adds the sources directory to Python Path
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 from utils.conscious_spending_plan import ConsciousSpendingPlan
+from utils.a_r_expenses import ARExpenses
 from utils.bank_export_retriever import BankExportRetriever
 
 
-bank_exports = BankExportRetriever('../data/').get_bank_exports()
-merged = pd.concat(bank_exports, ignore_index=True)
+parser = argparse.ArgumentParser(description='A CSV to Google Doc ETL that automates Monthly Finance Workflow.')
+parser.add_argument('--step2', dest='step2', action='store_true',
+                    help='Activates Step 2 of Workflow (After Manually Entering Shared Expenses)')
+parser.add_argument('--skip_next_month', dest='skip_next_month', action='store_true',
+                    help='Do not add a new worksheet for the next month.')
+args = parser.parse_args()
 
 csp = ConsciousSpendingPlan()
-csp.add_next_months_worksheet()
-csp.import_df(merged)
-# print(csp.export_df())
+if not args.step2:
+    print("Starting Step 1...")
+    bank_exports = BankExportRetriever('../data/').get_bank_exports()
+    merged = pd.concat(bank_exports, ignore_index=True)
+    if not args.skip_next_month:
+        csp.add_next_months_worksheet()
+    csp.import_df(merged)
+elif args.step2:
+    print("Starting Step 2...")
+    csp.update_worksheet_with_split_shared()
+    ARExpenses().import_df(csp.export_shared_only_df())
+print("Finished!")
 
 
 # from sklearn.datasets import load_iris
